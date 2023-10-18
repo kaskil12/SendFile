df2d3c4f07db8f20d9f6dd9200e29ea2fb04ee52
Kasper Kilde <kasperkilde2@gmail.com>
2023-10-18 11:47:15 +0200

    added file

---
+++ pyut/test.py
@@ -0,0 +1,33 @@
+import socket
+
+# Replace with the IP address and port to listen on
+listen_ip = '192.168.98.170'  # Listens on all available network interfaces
+listen_port = 3000
+
+
+# Replace with the path where you want to save the received file
+output_file_path = 'login.txt'
+
+def receive_file():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((listen_ip, listen_port))
+    server.listen(1)
+    
+    print(f"Listening on {listen_ip}:{listen_port}")
+
+    conn, addr = server.accept()
+    print(f"Connection from: {addr[0]}:{addr[1]}")
+
+    with open(output_file_path, 'wb') as file:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            file.write(data)
+    
+    conn.close()
+    server.close()
+    print(f"File received and saved as '{output_file_path}'")
+
+if __name__ == '__main__':
+    receive_file()
---
+++ pyut/testuy.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Specify the path to your Python script
+set python_script_path=""C:\Users\kaspe\OneDrive\Desktop\pyut\test.py""
+
+:: Execute the Python script
+python %python_script_path%
