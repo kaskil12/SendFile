f9486527fc991ed449d981a59536d2374ac23484
Kasper Kilde <kasperkilde2@gmail.com>
2023-10-18 10:29:41 +0200

    first push

---
+++ TEst/Open.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Specify the path to your C# program
+set csharp_program_path="C:\Users\kaspe\OneDrive\Desktop\TEst\FileSender.Cs"
+
+:: Execute the C# program
+start "" "%csharp_program_path%"
